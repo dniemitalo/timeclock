cf9f74d5f8cc10e407b1f8c43ec7c54324ce4cdd
Dan Niemitalo <dniemitalo@linnmar.k12.ia.us>
2016-11-12 07:22:37 -0600

    Added 30 day report
    Report lists hours in the last 30 days and totals the sum for each of
    the teams.

--- register.php
+++ register.php
@@ -13,13 +13,13 @@ require_once 'db.php';
 <?php
 $sql = "INSERT INTO students (ID, first, last) VALUES ($_POST[ID],'$_POST[first]','$_POST[last]')";
 if ($result = mysqli_query($conn,$sql)){
-?>
-<p>New user created successfully.</p>
-<p>Note: you are not punched in yet.</p>
-<p>Redirecting to time clock in 5 seconds...</p>
-<p><a href="http://www.nemoquiz.com/timeclock">Click here</a> if you are not redirected automatically.</p>
+	?>
+	<p>New user created successfully.</p>
+	<p>Note: you are not punched in yet.</p>
+	<p>Redirecting to time clock in 5 seconds...</p>
+	<p><a href="http://www.nemoquiz.com/timeclock">Click here</a> if you are not redirected automatically.</p>
 
-<?php
+	<?php
 }
 else{
 	echo mysqli_error($conn);
